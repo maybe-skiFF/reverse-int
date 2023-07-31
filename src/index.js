ccf799191bddc55472071a820d203dfbb5258b19
maybe-skiFF <den.skiff190@gmail.com>
2023-07-31 21:24:46 +0300

    complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let numToStr = String(Math.abs(n));
+  let res = '';
+
+  for (let i = 0; i < numToStr.length; i++) {
+    res = `${numToStr[i]}${res}`;
+  }
+  return +res;
 }
